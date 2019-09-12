6a44fe16ce502f6a958c928a64e5abeeceb8e190
Asotikov Anton <asant@yandex.ru>
2019-09-12 20:31:15 +0300

    Добавил домашку Extended-Syntax

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,5 +1,6 @@
+"use strict";
 
-
+// Задача 1
 function calculateQuadraticEquation(){
     let a = +window.a.value;
     let b = +window.b.value;
@@ -11,10 +12,20 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+    let discriminant = b ** 2 - 4 * a * c;
+    if (discriminant < 0) {
+        return [];
+    } else if (discriminant == 0) {
+        let x = -b / 2 * a;
+        return [x];
+    } else if (discriminant > 0) {
+        let x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        let x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        return [x1, x2];
+    }
 }
 
+// Задача 2
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
     let averageMark = getAverageMark(marks);
@@ -22,10 +33,21 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
+    if (marks.length > 5) {
+        console.warn("Количество оценок больше 5.");
+        marks.splice(5);
+    }
+
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+
+    let averageMark = sum / marks.length;
+    return averageMark;
 }
 
+// Задача 3
 function calculateDrinkTask(){
     let name = window.personName.value;
     let dateOfBirthday = new Date(window.dateOfBirthday.value);
@@ -34,7 +56,12 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
+    if (age >= 18) {
+        console.log(`Не желаете ли олд-фэшн, ${name}?`);
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    } else if (age < 18) {
+        console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
 }
